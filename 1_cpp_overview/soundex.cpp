dd3c05ef49352dde6ceb08fe6dd09e115016edd1
henriqueneffa <149350882+henriqueneffa@users.noreply.github.com>
2024-03-21 20:33:45 -0700

    Update soundex.cpp

--- 1_cpp_overview/soundex.cpp
+++ 1_cpp_overview/soundex.cpp
@@ -1,9 +1,3 @@
-/*
- * TODO: remove and replace this file header comment
- * This is a .cpp file you will edit and turn in.
- * Remove starter comments and add your own
- * comments on each function and on complex code sections.
- */
 #include <cctype>
 #include <fstream>
 #include <string>
